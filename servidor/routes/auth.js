39b6b3fd59cf7f9284d55bdab5b2f33ce29955c6
jesusjuvenalFidiasProg <94916847+jesusjuvenalFidiasProg@users.noreply.github.com>
2022-01-11 16:07:02 -0400

    first commit

---
+++ servidor/routes/auth.js
@@ -0,0 +1,16 @@
+//rutas para autentificar usuarios
+const express = require('express');
+const router = express.Router();
+const {check} = require('express-validator');
+const authController = require('../controllers/authController');
+
+//crear un usuario
+// api/auth
+
+router.post('/', 
+[
+    check('email', 'Agrega un email valido').isEmail(),
+    check('password', 'El password debe ser minimo de 6 caracteres').isLength({min : 6})
+], authController.autenticarUsuario );
+
+module.exports = router;
